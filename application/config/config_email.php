29d6a48fbcbbe2893724d6f29960d05b37757ed7
danielMonroy96 <danielmonroydominguez@gmail.com>
2019-07-08 00:09:27 -0500

    Issue#10

--- application/config/config_email.php
+++ application/config/config_email.php
@@ -2,6 +2,6 @@
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 $CI =& get_instance();
 $CI->load->model('reg/settings_model');
-$sInfo = $CI->settings_model->get("settings_email", 1)->row();
-$config['sender_email'] = $sInfo->email;
+$sInfo = $CI->settings_model->get("settings_id", 1)->row();
+$config['sender_email'] = $sInfo->settings_email;
 $config['sender_name']  = $sInfo->sender_name;
